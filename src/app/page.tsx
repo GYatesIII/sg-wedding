15ae963464c22d5b9e935dbb765a2b330bff6280
George Yates <me@georgeyatesiii.com>
2024-06-02 13:31:51 -0500

    Adding in logo to hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,28 +16,35 @@ export default function Home() {
       <Menu />
       <div className="relative w-full h-[100vh]">
         <div className="absolute top-0 left-0 bottom-0 right-0 z-30 flex flex-col justify-center items-center gap-5">
-          <Subheading className="text-center text-2xl lg:text-6xl">
+          <div className="w-[240px] mb-2 lg:w-[600px] lg:mb-14">
+            <Image
+              src="/sarah-george-logo.png"
+              alt="Sarah G"
+              width={1080}
+              height={646}
+            />
+          </div>
+          <Subheading className="mb-0">
             Sarah &amp; George get hitched
           </Subheading>
-          {/* <Image src="/next.svg" alt="Sarah G" width={200} height={200} /> */}
-          <p className="text-center text-white text-3xl lg:text-6xl font-headline mt-4 mb-4">
+          <p className="text-center text-white text-3xl lg:text-6xl font-headline mt-0 mb-2 lg:mb-4">
             December 13 & 14, 2024
           </p>
           <div className="flex">
-            <div className="hidden lg:block h-full w-28 bg-gradient-to-l from-[rgba(255,255,255,0.8)] to-transparent"></div>
-            <div className="w-full lg:w-[inherit] bg-white bg-opacity-80">
+            <div className="hidden lg:block h-full w-28 bg-gradient-to-l from-[rgba(255,255,255,0.85)] to-transparent"></div>
+            <div className="w-[100vw] lg:w-[inherit] bg-white bg-opacity-85">
               <Countdown />
             </div>
-            <div className="hidden lg:block h-full w-28 bg-gradient-to-r from-[rgba(255,255,255,0.8)] to-transparent"></div>
+            <div className="hidden lg:block h-full w-28 bg-gradient-to-r from-[rgba(255,255,255,0.85)] to-transparent"></div>
           </div>
 
-          <p className="text-white text-2xl lg:text-4xl font-headline mt-5 text-center">
+          <p className="text-white text-2xl lg:text-4xl font-headline mt-0 lg:mt-5 text-center">
             Hacienda Sotuta de Peón
             <br />
             <span className="text-xl lg:text-2xl">near Mérida, México</span>
           </p>
         </div>
-        <div className="absolute top-0 left-0 bottom-0 right-0 z-20 bg-black bg-opacity-20"></div>
+        <div className="absolute top-0 left-0 bottom-0 right-0 z-20 bg-black bg-opacity-35"></div>
         <div className="absolute top-0 left-0 bottom-0 right-0 z-10 bg-sg-orange bg-opacity-20"></div>
         <div className="absolute top-0 left-0 bottom-0 right-0 z-0">
           <video
